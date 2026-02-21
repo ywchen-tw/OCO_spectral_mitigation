604534f9f5bf0277dd661e90d2bb5a73a667bed9
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-20 18:59:25 -0700

    orbit id update

--- src/abs_util/fp_atm.py
+++ src/abs_util/fp_atm.py
@@ -168,26 +168,7 @@ def oco_fp_atm_abs(sat=None, o2mix=0.20935, output='fp_tau_{}.h5',
         print("lat_l1b_select size: ", lat_l1b_select.shape)
         print("sfc_p_mask size: ", sfc_p_mask.shape)
         if lat_l1b_select.shape[0] != sfc_p_mask.shape[0]:
-            print("Mismatch in number of soundings between L1B and selection mask!")
-            print("Checking L1B TG file...")
-
-            if 'oco_l1b_tg' in oco_files_dict.keys():
-                with h5py.File(oco_files_dict['oco_l1b_tg'], 'r') as oco_l1b_tg:
-                    lon_l1b_tg = oco_l1b_tg['SoundingGeometry/sounding_longitude'][...]
-                    lat_l1b_tg = oco_l1b_tg['SoundingGeometry/sounding_latitude'][...]
-                    tg_logic = np.isfinite(lon_l1b_tg) & np.isfinite(lat_l1b_tg)
-                    lat_l1b_tg_select = lat_l1b_tg[tg_logic]
-                    snd_id_l1b_tg = oco_l1b_tg['SoundingGeometry/sounding_id'][...][tg_logic]
-                    v_solar_tg = oco_l1b_tg['SoundingGeometry/sounding_solar_relative_velocity'][...][tg_logic]
-                    v_inst_tg = oco_l1b_tg['SoundingGeometry/sounding_relative_velocity'][...][tg_logic]
-                    dist_au_tg = oco_l1b_tg['SoundingGeometry/sounding_solar_distance'][...][tg_logic] / AU_M
-                    
-                    print("lat_l1b_tg shape: ", lat_l1b_tg.shape)
-                    print("lat_l1b_tg flat + lat_l1b_select flat length sum: ", lat_l1b_tg.flatten().shape[0] + lat_l1b_select.flatten().shape[0])
-            else:
-                print("[Error] No L1B TG file provided for debugging!")
-                
-            
+            print("Mismatch in number of soundings between L1B and selection mask!")            
             sys.exit(1)
             
         # mask fp_id not in oco_l1b_snd_id
