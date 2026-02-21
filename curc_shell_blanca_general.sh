6e78d2bc68c4af5ed81fccb9478c3c7994625f2f
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-20 19:05:34 -0700

    orbit id update

--- curc_shell_blanca_general.sh
+++ curc_shell_blanca_general.sh
@@ -46,8 +46,8 @@ cd /projects/yuch8913/OCO_spectral_mitigation
 # Specify year, month, and day ranges
 start_year=2020
 end_year=2020
-start_month=12
-end_month=12
+start_month=9
+end_month=9
 start_day=1
 end_day=1
 
