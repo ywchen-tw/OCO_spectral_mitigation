a6b2f1a59e4714b940d8037f76c8faf11d4e3f1a
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-22 10:24:11 -0700

    cloud mask update

--- src/oco_fp_spec_anal.py
+++ src/oco_fp_spec_anal.py
@@ -1019,12 +1019,7 @@ def preprocess(target_date, data_dir="data", result_dir="results", limit_granule
         sat0[orbit_id] = {}
         for file in glob.glob(f"{OCO2_data_dir}/{orbit_id}/*"):
             if "L1b" in file:
-                if "TG" not in file:
-                    print(f"Found L1b file for orbit {orbit_id}: {file}")
-                    sat0[orbit_id]["oco_l1b"] = file
-                elif "TG" in file:
-                    print(f"Found L1b TG file for orbit {orbit_id}: {file}")
-                    sat0[orbit_id]["oco_l1b_tg"] = file
+                sat0[orbit_id]["oco_l1b"] = file
             if "Met" in file:
                 sat0[orbit_id]["oco_met"] = file
             if "CPr" in file:
