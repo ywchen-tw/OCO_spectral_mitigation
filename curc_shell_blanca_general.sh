18904f291968c72c24e2e87ee6ddafe9a279c505
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-22 14:15:18 -0700

    cloud mask update

--- curc_shell_blanca_general.sh
+++ curc_shell_blanca_general.sh
@@ -46,10 +46,10 @@ cd /projects/yuch8913/OCO_spectral_mitigation
 # Specify year, month, and day ranges
 start_year=2020
 end_year=2020
-start_month=9
-end_month=9
-start_day=3
-end_day=3
+start_month=1
+end_month=1
+start_day=1
+end_day=1
 
 # Loop through year, month, day
 for year in $(seq $start_year $end_year); do
