4f4fdacaaead1dd50e799b2afd0b4a6f94711bd9
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-23 10:46:49 -0700

    cld distance update

--- curc_shell_cld_dist_blanca_general.sh
+++ curc_shell_cld_dist_blanca_general.sh
@@ -44,10 +44,10 @@ cd /projects/yuch8913/OCO_spectral_mitigation
 # Specify year, month, and day ranges
 start_year=2020
 end_year=2020
-start_month=10
-end_month=10
-start_day=5
-end_day=5
+start_month=12
+end_month=12
+start_day=24
+end_day=24
 
 # Loop through year, month, day
 for year in $(seq $start_year $end_year); do
