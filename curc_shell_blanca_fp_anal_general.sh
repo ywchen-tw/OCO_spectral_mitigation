29bbbfd2a63101146e7f3eb3d30f842183ac220c
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-23 17:50:37 -0700

    modis match update

--- curc_shell_blanca_fp_anal_general.sh
+++ curc_shell_blanca_fp_anal_general.sh
@@ -37,10 +37,10 @@ cd /projects/yuch8913/OCO_spectral_mitigation
 # Option 1: Loop with year, month, day (ACTIVE)
 # ============================================================================
 # Specify year, month, and day ranges
-start_year=2020
-end_year=2020
-start_month=12
-end_month=12
+start_year=2021
+end_year=2021
+start_month=4
+end_month=4
 start_day=24
 end_day=24
 
